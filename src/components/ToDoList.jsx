1350ee5f6fb1837ef440a4851e3ed056bc04764c
s0m3us3r <132075491+s0m3us3r@users.noreply.github.com>
2024-03-04 15:53:44 +0200

    typo fix

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
-import "ag-grid-community/styles/ag-theme-material                                            .css"; // Optional Theme applied to the grid
+import "ag-grid-community/styles/ag-theme-material.css"; // Optional Theme applied to the grid
 import dayjs from 'dayjs';
 
 function ToDoList() {
